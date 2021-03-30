b4084342ba30d001d2f231673c4a780e7cdeafa2
ashwinisapreknoxpo <ashwini.sapre@knoxpo.com>
2021-03-30 12:07:27 +0530

    Initial Commit

---
+++ DineApp/DineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DineAppApp.swift
+//  DineApp
+//
+//  Created by Knoxpo MacBook Pro on 30/03/21.
+//
+
+import SwiftUI
+
+@main
+struct DineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
